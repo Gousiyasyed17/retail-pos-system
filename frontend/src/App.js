9f2a4412722332167cd2d41a26052fd12460ed01
Gousiyasyed17 <gousiyaparveen2006@gmail.com>
2026-03-24 23:59:39 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import ProductManagement from "./pages/ProductManagement";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const user = localStorage.getItem("loggedInUser");
-    if (user) {
-      setLoggedIn(true);
-    }
-  }, []);
-
-  const handleLogin = (username) => {
-    localStorage.setItem("loggedInUser", username);
-    setLoggedIn(true);
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem("loggedInUser");
-    setLoggedIn(false);
-  };
-
   return (
     <div className="App">
-      {loggedIn ? (
-        <Dashboard onLogout={handleLogout} />
-      ) : (
-        <Login onLogin={handleLogin} />
-      )}
+      <ProductManagement />
     </div>
   );
 }
